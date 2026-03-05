68bfcb72eaca166cb7a369363d64746016100dc0
Pete <pbay100@aol.com>
2026-03-05 18:17:26 +0000

    Initial commit - Python Quest project setup

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("startBtn").addEventListener("click", function() {
+    alert("Lesson 1: Variables\n\nIn Python you can store data in variables.\nExample:\n\nname = 'Pete'");
+});
